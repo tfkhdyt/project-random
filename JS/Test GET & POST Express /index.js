dc8592faedcb61aa611a3bd68d7e09b3ab6b1de3
tfkhdyt <ta142fa@gmail.com>
2021-11-06 08:00:15 +0700

    nambah endpoint patch

--- JS/Test GET & POST Express /index.js
+++ JS/Test GET & POST Express /index.js
@@ -5,29 +5,38 @@ const PORT = process.env.port || 3000;
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const capitalize = (text) => {
-  return text.split(' ').map((e) => {
+  return text
+  .split(' ')
+  .map((e) => {
     return e[0].toUpperCase() + e.substring(1).toLowerCase();
-  }).join(' ');
+  })
+  .join(' ');
 };
 
 const user = [{
   nama: 'Taufik Hidayat',
   nim: '301200032',
-  prodi: 'Teknik Informatika'
-}, {
-  nama: 'Teja Kusumah',
-  nim: '301200033',
-  prodi: 'Teknik Informatika'
-}];
+  prodi: 'Teknik Informatika',
+},
+  {
+    nama: 'Teja Kusumah',
+    nim: '301200033',
+    prodi: 'Teknik Informatika',
+  },
+];
 
 const keyValidation = (req, res, next) => {
   if (req.query.key != 'QNkRd2Q2sTu2r') {
     res.status(401);
     return res.json({
-      message: 'API Key tidak valid!'
+      message: 'API Key tidak valid!',
     });
   }
   next();
@@ -35,11 +44,11 @@ const keyValidation = (req, res, next) => {
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Homepage'
+    title: 'Homepage',
   });
 });
 
-app.get('/get/user', keyValidation, (req, res) => {
+app.get('/user', keyValidation, (req, res) => {
   const nim = req.query.nim;
   const fetchedData = user.find((e) => e.nim == nim);
   let json = {
@@ -47,10 +56,10 @@ app.get('/get/user', keyValidation, (req, res) => {
     data: {
       nama: null,
       nim: null,
-      prodi: null
-    }
+      prodi: null,
+    },
   };
-  
+
   if (fetchedData) {
     res.status(200);
     json.message = 'Data berhasil ditemukan!';
@@ -62,7 +71,41 @@ app.get('/get/user', keyValidation, (req, res) => {
     json.message = 'Data tidak ditemukan!';
     delete json.data;
   }
+
+  res.json(json);
+});
+
+app.patch('/user', keyValidation, (req, res) => {
+  const nim = req.query.nim;
+  const fetchedData = user.find((e) => e.nim == nim);
+  const nama = req.body.nama || fetchedData.nama;
+  const prodi = req.body.prodi || fetchedData.prodi;
+  const updatedData = user.map((e) => {
+    if (e.nim == nim) {
+      return {
+        ...e,
+        nama,
+        prodi
+      };
+    } else {
+      return e;
+    }
+  });
   
+  console.log(updatedData);
+  
+  let json = {};
+
+  if (updatedData) {
+    res.status(200);
+    json.message = 'Data berhasil diubah';
+    json.data = updatedData;
+  } else {
+    res.status(404);
+    json.message = 'Data tidak ditemukan!';
+    delete json.data;
+  }
+
   res.json(json);
 });
 
