ec1adc87301264c529be4f2632fe06cd75faab27
tfkhdyt <ta142fa@gmail.com>
2021-11-22 22:01:45 +0700

    nambah project parse data from lutris api

---
+++ JS/parseDataFromLutris/index.js
@@ -0,0 +1,15 @@
+const { get } = require('axios');
+
+(async () => {
+  const link = process.argv[2];
+  const data = await get(link)
+    .then((res) => res.data)
+    .then((data) => data.results[0]);
+  // console.log(data);
+  console.log(`Judul game  : ${data.name} 
+Tahun rilis : ${data.year} 
+Uploader    : ${data.user} 
+Versi       : ${data.version} 
+Catatan     :
+${data.notes}`);
+})();
