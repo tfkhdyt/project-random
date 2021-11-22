f5e423754e8de6f4e5bfb64f3a801a9938313ddd
tfkhdyt <ta142fa@gmail.com>
2021-11-23 05:11:52 +0700

    build release

---
+++ rust/guessing_game/target/release/build/libc-268e669f8c652229/build_script_build-268e669f8c652229.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/build/libc-268e669f8c652229/build_script_build-268e669f8c652229: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs
+
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/build/libc-268e669f8c652229/build_script_build-268e669f8c652229.d: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs
+
+/data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs:
---
+++ rust/guessing_game/target/release/deps/guessing_game-d5621c5e63353fd5.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/guessing_game-d5621c5e63353fd5: src/main.rs
+
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/guessing_game-d5621c5e63353fd5.d: src/main.rs
+
+src/main.rs:
