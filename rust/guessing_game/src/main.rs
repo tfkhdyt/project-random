a26328b6fced79cc947bf397414e78d31313b77b
tfkhdyt <ta142fa@gmail.com>
2021-11-23 04:28:58 +0700

    nambah fitur input pada guessing_game

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Tebak angkanya!");
+    println!("Masukkan tebakan mu.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("Kamu menebak: {}", guess);
 }
