25db18d6137f0fcf2ffb4a5529b97798cdebcf92
tfkhdyt <ta142fa@gmail.com>
2021-11-23 05:06:19 +0700

    menambah looping

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -6,16 +6,23 @@ fn main() {
     println!("Tebak angkanya!");
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("Angka rahasianya adalah: {}", secret_number);
-    println!("Masukkan tebakan mu: ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Mohon masukkan angka!");
-    println!("Kamu menebak: {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Terlalu kecil!"),
-        Ordering::Greater => println!("Terlalu besar!"),
-        Ordering::Equal => println!("Kamu menang!"),
-    }
-}
+    loop {
+        println!("Masukkan tebakan mu: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Kamu menebak: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Terlalu kecil!"),
+            Ordering::Greater => println!("Terlalu besar!"),
+            Ordering::Equal => {
+                println!("Kamu menang!");
+                break;
+            }
+        }
+    }}
