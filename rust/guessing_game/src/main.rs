2e9a0f550a7cb6390fc95e998e2bf35787757a82
tfkhdyt <ta142fa@gmail.com>
2021-11-23 04:43:16 +0700

    nampilin hasil rng

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Tebak angkanya!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Angka rahasianya adalah: {}", secret_number);
     println!("Masukkan tebakan mu: ");
     let mut guess = String::new();
     io::stdin()
