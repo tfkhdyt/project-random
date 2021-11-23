f073c39b074fe36b0da179c700ba2aabbf504292
tfkhdyt <ta142fa@gmail.com>
2021-11-23 09:18:13 +0700

    rename guessing game

---
+++ rust/guessing_game_/target/release/deps/rand_chacha-8cfe6e406f1000b1.d
@@ -0,0 +1,9 @@
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/rand_chacha-8cfe6e406f1000b1.rmeta: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/librand_chacha-8cfe6e406f1000b1.rlib: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/rand_chacha-8cfe6e406f1000b1.d: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
+/data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
+/data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
---
+++ rust/guessing_game_/target/release/guessing_game.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/project/rust/guessing_game/target/release/guessing_game: /data/data/com.termux/files/home/project/rust/guessing_game/src/main.rs
