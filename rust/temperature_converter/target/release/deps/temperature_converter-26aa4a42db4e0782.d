2c37aabe16e6137469a2836cb4124d898e979c30
tfkhdyt <ta142fa@gmail.com>
2021-11-23 11:30:28 +0700

    build release

---
+++ rust/temperature_converter/target/release/deps/temperature_converter-26aa4a42db4e0782.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/project/rust/temperature_converter/target/release/deps/temperature_converter-26aa4a42db4e0782: src/main.rs
+
+/data/data/com.termux/files/home/project/rust/temperature_converter/target/release/deps/temperature_converter-26aa4a42db4e0782.d: src/main.rs
+
+src/main.rs:
---
+++ rust/temperature_converter/target/release/temperature_converter.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/project/rust/temperature_converter/target/release/temperature_converter: /data/data/com.termux/files/home/project/rust/temperature_converter/src/main.rs
