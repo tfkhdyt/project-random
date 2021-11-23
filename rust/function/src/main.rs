5789a82dc358871bbb1ca7ea9024c7985c518aad
tfkhdyt <ta142fa@gmail.com>
2021-11-23 07:50:23 +0700

    finish project functions

--- rust/function/src/main.rs
+++ rust/function/src/main.rs
@@ -1,3 +1,18 @@
+fn sebut_nama(nama: String) {
+    println!("Nama saya adalah {}", nama);
+}
+
+fn lima() -> u8 {
+    5
+}
+
+fn tambah(x: isize, y: isize) -> isize {
+    x + y
+}
+
 fn main() {
     println!("Hello, world!");
+    sebut_nama("Taufik".to_string());
+    println!("Nilai dari x adalah: {}", lima());
+    println!("{} + {} = {}", 6, 9, tambah(6, 9));
 }
---
+++ rust/function/target/debug/function.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/project/rust/function/target/debug/function: /data/data/com.termux/files/home/project/rust/function/src/main.rs
