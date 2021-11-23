4cd834f45a0e7f40c3f79ac265ca1ba47419d939
tfkhdyt <ta142fa@gmail.com>
2021-11-23 08:53:24 +0700

    gak jadi bikin submodule

--- rust/_guessing_game/target/release/deps/cfg_if-2b02e217fb238879.d
+++
@@ -1,7 +0,0 @@
-/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/cfg_if-2b02e217fb238879.rmeta: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/libcfg_if-2b02e217fb238879.rlib: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/data/data/com.termux/files/home/project/rust/guessing_game/target/release/deps/cfg_if-2b02e217fb238879.d: /data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/data/data/com.termux/files/home/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
