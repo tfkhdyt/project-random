a2d81c26636a451072b8abb1f4c0b616f6330ab4
tfkhdyt <ta142fa@gmail.com>
2021-11-23 08:44:14 +0700

    menambah fitur percobaan

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,18 +1,33 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::io::Write;
 
 fn main() {
-    println!("Tebak angkanya!");
+    // deklarasi variabel
+    let mut percobaan = 5;
     let secret_number = rand::thread_rng().gen_range(1..101);
-    // println!("Angka rahasianya adalah: {}", secret_number);
-    println!("bruh");
+
+    println!("Tebak angkanya!");
     loop {
-        println!("Masukkan tebakan mu: ");
+        if percobaan == 0 {
+            println!(
+                "Sisa percobaan anda telah habis!\n\
+                Anda kalah!\n\
+                Jawabannya adalah: {}",
+                secret_number
+            );
+            break;
+        }
+        // input
+        print!("Masukkan tebakan mu: ");
+        io::stdout().flush().unwrap();
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
+
+        // ubah input menjadi integer
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -20,10 +35,25 @@ fn main() {
                 continue;
             }
         };
-        // println!("Kamu menebak: {}", guess);
+
+        // mengecek apakah input sesuai dengan secret number
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Terlalu kecil!\n"),
-            Ordering::Greater => println!("Terlalu besar!\n"),
+            Ordering::Less => {
+                percobaan -= 1;
+                println!(
+                    "Terlalu kecil!\n\
+                    Sisa percobaan anda: {}\n",
+                    percobaan
+                );
+            }
+            Ordering::Greater => {
+                percobaan -= 1;
+                println!(
+                    "Terlalu besar!\n\
+                    Sisa percobaan anda: {}\n",
+                    percobaan
+                );
+            }
             Ordering::Equal => {
                 println!("Kamu menang!");
                 break;
