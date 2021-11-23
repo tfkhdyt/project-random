1f4c1489eae8070aa21fe7f79b2d46fdd17b930f
tfkhdyt <ta142fa@gmail.com>
2021-11-23 10:30:46 +0700

    compile project "branches"

--- rust/branches/src/main.rs
+++ rust/branches/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 7;
+
+    if number < 5 {
+        println!("Kondisinya benar!");
+    } else {
+        println!("Kondisinya salah!");
+    }
+
+    let condition = true;
+    let number = if condition { 6 } else { 9 };
+    println!("The value of the number is {}", number);
 }
