9102c1f8c6b1369654e11a7f32818b67d8e54b7f
tfkhdyt <ta142fa@gmail.com>
2021-11-24 22:17:46 +0700

    nambah project greeting

--- JavaScript/input/index.js
+++ JavaScript/input/index.js
@@ -1,7 +1,17 @@
 const rl = require('readline-sync');
 
+const getTime = (hours) => {
+  if (hours >= 3 && hours <= 10) return 'morning';
+  else if (hours >= 11 && hours <= 3) return 'afternoon';
+  else if (hours >= 4 && hours <= 6) return 'evening';
+  else return 'night';
+};
+
 (async () => {
   const name = await rl.question("What's your name? ");
-  console.log(`Good morning, ${name}!`);
-  // rl.close();
+
+  const hours = new Date().getHours();
+  const time = getTime(hours);
+
+  console.log(`Good ${time}, ${name}!`);
 })();
