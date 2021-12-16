57f629f6bd45c87fc73aae5653860f39bc0de079
tfkhdyt <ta142fa@gmail.com>
2021-12-16 21:11:05 +0700

    hapus beberapa project

--- JavaScript/Mei 2021/swal/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-// const path = require('path');
-const swal = require('sweetalert');
-const app = express();
-
-app.use(express.static(__dirname+'/public'));
-
-app.listen(5000, console.log('Server is running on port 5000'));
