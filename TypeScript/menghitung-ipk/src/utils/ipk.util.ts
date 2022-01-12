3ba09d1bba6c6638348efb4627a97bc748d6fa94
tfkhdyt <ta142fa@gmail.com>
2022-01-12 20:25:15 +0700

    prettier

--- TypeScript/menghitung-ipk/src/utils/ipk.util.ts
+++ TypeScript/menghitung-ipk/src/utils/ipk.util.ts
@@ -10,11 +10,8 @@ export const getIpk = (dataSemuaNilai: DataNilai[]): number => {
 }
 
 export const getPredikat = (ipk: number): string => {
-  if (ipk >= 3.5 && ipk <= 4)
-    return 'DENGAN PUJIAN'
-  if (ipk >= 3 && ipk <= 3.49)
-    return 'SANGAT MEMUASKAN'
-  if (ipk >= 2.5 && ipk <= 2.99)
-    return 'MEMUASKAN'
+  if (ipk >= 3.5 && ipk <= 4) return 'DENGAN PUJIAN'
+  if (ipk >= 3 && ipk <= 3.49) return 'SANGAT MEMUASKAN'
+  if (ipk >= 2.5 && ipk <= 2.99) return 'MEMUASKAN'
   return 'LULUS'
 }
--- TypeScript/menghitung-ipk/src/utils/nilai.util.ts
+++ TypeScript/menghitung-ipk/src/utils/nilai.util.ts
@@ -1,21 +1,15 @@
 import { BobotAngka, NilaiHuruf } from '../libs/enums.lib'
 
 export const getBobotAngka = (nilai: number): BobotAngka => {
-  if (nilai >= 80 && nilai <= 100) 
-    return BobotAngka.EMPAT
-  if (nilai >= 65 && nilai <= 79) 
-    return BobotAngka.TIGA
-  if (nilai >= 55 && nilai <= 64) 
-    return BobotAngka.DUA
+  if (nilai >= 80 && nilai <= 100) return BobotAngka.EMPAT
+  if (nilai >= 65 && nilai <= 79) return BobotAngka.TIGA
+  if (nilai >= 55 && nilai <= 64) return BobotAngka.DUA
   return BobotAngka.NOL
 }
 
 export const getNilaiHuruf = (nilai: number): NilaiHuruf => {
-  if (nilai >= 80 && nilai <= 100) 
-    return NilaiHuruf.A
-  if (nilai >= 65 && nilai <= 79) 
-    return NilaiHuruf.B
-  if (nilai >= 55 && nilai <= 64) 
-    return NilaiHuruf.C
+  if (nilai >= 80 && nilai <= 100) return NilaiHuruf.A
+  if (nilai >= 65 && nilai <= 79) return NilaiHuruf.B
+  if (nilai >= 55 && nilai <= 64) return NilaiHuruf.C
   return NilaiHuruf.D
 }
