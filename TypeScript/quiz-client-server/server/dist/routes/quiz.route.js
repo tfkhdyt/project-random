cc85fd8c332145500fc41db8fdfab61356b8a35a
tfkhdyt <ta142fa@gmail.com>
2022-02-11 07:36:43 +0700

    hapus project quiz

--- TypeScript/quiz-client-server/server/dist/routes/quiz.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const quiz_controller_1 = require("../controllers/quiz.controller");
-const router = (0, express_1.Router)();
-router.get('/questions', quiz_controller_1.getQuestions);
-router.get('/questions/:id', quiz_controller_1.getQuestion);
-router.post('/answer/:id', quiz_controller_1.answerQuestion);
-exports.default = router;
