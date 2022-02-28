aa1d4b3429362a753dc48229ad6d1a3dc9d9c9a2
tfkhdyt <ta142fa@gmail.com>
2022-02-28 19:12:19 +0700

    add comment to split-string-pascal-case.js

--- JavaScript/split-string-pascal-case.js
+++ JavaScript/split-string-pascal-case.js
@@ -4,13 +4,26 @@ let str = "SelamatPagiDunia"
 
 // Jawaban:
 function manipulateString(st) {
+  // buat variabel dengan string kosong
   let string = ''
+
+  // lakukan perulangan untuk variabel "st" lalu simpan setiap karakter pada variabel "char"
   for (let char of st) {
+
+    // apabila variabel "char" adalah huruf kapital
     if (char === char.toUpperCase()) 
+      
+      // concat variabel "string" dengan spasi dan variabel "char"
       string += ` ${char}`
+    
+    // apabila tidak
     else
+
+      // concat variabel "string" dengan variabel "char"
       string += char
   }
+
+  // return variabel string dengan dimulai dari index ke-1
   return string.slice(1)
 }
 
